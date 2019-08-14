66135304a67c68f9555d442b429fe50c314369a4
skennel2 <skennel2@gmail.com>
2019-08-14 16:08:07 +0900

    reduce, action 작성

---
+++ src/components/reducers/index.js
@@ -0,0 +1,54 @@
+import { combineReducers } from 'redux';
+
+const initialState = {
+    items : [
+        {
+            id : 1,
+            todo : 'go shopping',
+            isComplete : false
+        }
+    ],
+    showNoInputAlert : false
+}
+
+const reducer = function(state=initialState, action){
+    if(action.type === 'ADD_TODO'){
+        if(action.payload.todo.trim() === 0){
+            return {
+                ...state,
+                showNoInputAlert : true
+            }
+        } 
+        const newItems = state.items.concat(action.payload);
+
+        return {
+            ...state,
+            items : newItems
+        };
+    }else if(action.type === 'DELETE_TODO'){
+        const newItems = state.items.filter(item => item.id !== action.payload);
+
+        return {
+            ...state,
+            items : newItems
+        };
+    }else if(action.type === 'COMPLETE_TODO'){
+        const newItems = state.items.map(item => item.id === action.payload ? {
+            ...item,
+            isComplete : true
+        } : item);
+
+        return {
+            ...state,
+            items : newItems
+        };
+    }else{
+        return state;
+    }
+}
+
+const reducers = combineReducers({
+    reducer
+}); 
+
+export default reducers;
